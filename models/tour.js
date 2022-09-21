87ad8373e8a2229d90bc5b1b0d60f28ba9f25679
jakiasultana2022 <rokeyasbd@gmail.com>
2022-09-21 19:45:32 +0600

    tours

---
+++ models/tour.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const tourSchema =  mongoose.Schema({
+    name:{
+      type: String,
+      required: [true,"Please provide a name for this tour"],
+      trim: true,
+      unique:true,
+      minLength:[3,"Name must be at least 3 characters."],
+      maxLength:[100,"Name is too large"]
+    },
+  
+    description:{
+      type: String,
+      required: true,
+    },
+  
+    image:{
+      type: String
+    },
+  
+    price:{
+      type: Number,
+      required: true,
+      min:[0,"Price cant't be negative"],
+    
+    },
+    viewer: {
+        type: Number,
+        default: 0,
+        required: true
+    }
+}, {
+    timestamps: true,
+})
+    
+
+
+  const Tours = mongoose.model("Tours", tourSchema);
+
+module.exports = Tours;
